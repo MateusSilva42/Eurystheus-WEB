7bb2e7a9f04b50b6533b3d8fdf08b3b244c48a1f
mateus silva <mateus42silva@gmail.com>
2024-04-08 22:16:32 -0300

    create login and register layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,50 @@
 <template>
-  <v-toolbar color="warning" flat dense :elevation="3" title="Eurystheus">
-    <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn icon v-bind="props" @click="notify">
-          <v-icon>mdi-logout</v-icon>
-        </v-btn>
-      </template>
-    </v-menu>
+  <v-toolbar class="toolbar" color="#032e51" flat dense :elevation="3">
+    <div class="toolbar-left">
+      <v-img src="~/assets/logo.jpg" max-height="40" contain></v-img>
+      <v-toolbar-title v-if="!isSmallScreen">Eurystheus</v-toolbar-title>
+    </div>
+
+    <v-spacer></v-spacer>
+    <div class="toolbar-right">
+      <v-toolbar-title>Olá, @Mateus</v-toolbar-title>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props" @click="notify">
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </template>
+      </v-menu>
+    </div>
+
   </v-toolbar>
 
   <v-container>
     <v-card variant="outlined">
       <v-row>
         <v-col cols="12">
-            <v-tabs v-model="tab" bg-color="warning" fixed-tabs :vertical="true" center-active show-arrows>
-                <v-tab value="all">Todas as tarefas</v-tab>
-                <v-tab value="done">Concluídas</v-tab>
-                <v-tab value="pendent">Pendentes</v-tab>
-            </v-tabs>
+          <v-tabs v-model="tab" bg-color="#ff4500" fixed-tabs :vertical="true" center-active show-arrows>
+            <v-tab value="all">Todas as tarefas</v-tab>
+            <v-tab value="done">Concluídas</v-tab>
+            <v-tab value="pendent">Pendentes</v-tab>
+          </v-tabs>
         </v-col>
-      <v-card-text>
-        <v-window v-model="tab">
-          <v-window-item value="all">
-            <TaskList key="all" title="Todas as tarefas" scope="all" :tab="tab" />
-          </v-window-item>
+        <v-card-text>
+          <v-window v-model="tab">
+            <v-window-item value="all">
+              <TaskList key="all" title="Todas as tarefas" scope="all" :tab="tab" />
+            </v-window-item>
 
-          <v-window-item value="done">
+            <v-window-item value="done">
               <TaskList key="done" title="Tarefas concluídas" scope="done" :tab="tab" />
-          </v-window-item>
+            </v-window-item>
 
-          <v-window-item value="pendent">
+            <v-window-item value="pendent">
               <TaskList key="pendent" title="Tarefas pendentes" scope="pendent" :tab="tab" />
-          </v-window-item>
-        </v-window>
-      </v-card-text>
-    </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card-text>
+      </v-row>
     </v-card>
   </v-container>
 
@@ -46,37 +56,57 @@
 <script setup lang="ts">
 import { useDisplay } from 'vuetify';
 
-    let tab = ref('');
-    const display = useDisplay();
+let tab = ref('');
+const display = useDisplay();
 
-    const notify = () => {
-      const toast = useToast();
-      toast.success("Usuário deslogou com sucesso");
-    };
+const notify = () => {
+  const toast = useToast();
+  toast.success("Usuário deslogou com sucesso");
+};
 
-    const handleLogout = (item: { action: string }) => {
-      console.log("Usuário fez logout");
-    };
+const handleLogout = (item: { action: string }) => {
+  console.log("Usuário fez logout");
+};
 
-    const getUserTasks = async () => {
-      try {
-        const response = await useApi("task/6611e9299cd0a450907248e4", {
-          method: "GET",
-        });
-        console.log(response);
-      } catch (error) {
-        console.error("Erro ao buscar dados:", error);
-      }
-    };
-
-    const isSmallScreen = computed(() => {
-      return display.mobile.value || display.xs.value;
+const getUserTasks = async () => {
+  try {
+    const response = await useApi("task/6611e9299cd0a450907248e4", {
+      method: "GET",
     });
+    console.log(response);
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+  }
+};
+
+const isSmallScreen = computed(() => {
+  return display.mobile.value || display.xs.value;
+});
 
 </script>
 
 <style>
 .v-slide-group__content {
   color: black;
+}
+
+.toolbar {
+  background-color: #032e51;
+  display: flex;
+  justify-content: flex-start;
+}
+
+.toolbar-left {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 20%;
+}
+
+.toolbar-right {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 20%;
 }
 </style>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,14 +1,110 @@
 <template>
-    <h1>
-        Página de Login
-    </h1>
+        <v-row class="login-container">
+            <v-col class="login-form" cols="12" md="4" >
+                <v-row class="login-form-container">
+                    <v-col class="flex-center app-name-container">
+                        <span class="app-name">Eurystheus</span>
+                    </v-col>
+                    <v-col class="flex-center">
+                        <v-img src="~/assets/logo.jpg" max-height="100" max-width="100"></v-img>
+                    </v-col>
+                    <v-col>
+                        <v-card class="card-login">
+                    <v-card-title class="card-title login-title" >Login</v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field label="Usuário" v-model="user" outlined></v-text-field>
+                            <v-text-field label="Senha" v-model="password" outlined></v-text-field>
+                            <v-btn color="primary" @click="login">Entrar</v-btn>
+                        </v-form>
+                        <p class="login-text">Não possui conta? se <a href="/login/register"> cadastre</a></p>
+                    </v-card-text>
+                </v-card>
+                    </v-col>
+                </v-row>
+                
+
+            </v-col>
+            <v-col class="login-bg" cols="8" v-if="!isSmallScreen">
+            </v-col>
+        </v-row>
 
 </template>
 
-<script>
+<script setup lang="ts">
+import { useDisplay } from 'vuetify';
+const display = useDisplay();
 
+const isSmallScreen = computed(() => {
+    return display.mobile.value || display.xs.value;
+});
 </script>
 
-<style>
+<style scoped>
+
+.login-container {
+    display: flex;
+    padding: 0!important;
+    margin: 0!important;
+}
+
+.login-form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #032e51;
+    padding: 20px;
+    border-radius: 5px;
+}
+
+.login-bg {
+    background-image: url('~/assets/login-img.jpg');
+    background-size: cover;
+    background-position: center;
+    
+    border-radius: 5px;
+}
+
+.card-login {
+    width: 100%;
+    height: 300px;
+}
+
+.login-title{
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+
+.login-form-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+}
+
+.flex-center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.app-name {
+    font-size: 2rem;
+    font-weight: bold;
+    color: white;
+}
+
+.app-name-container {
+    margin-bottom: 20px;
+}
+
+.login-text {
+    padding: 10px;
+text-align: center;
+}
+
 
 </style>
